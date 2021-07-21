3ec38139e9dab03804eadd86771e107ff8a22743
tamiisoledad <tamiisoledad4@gmail.com>
2021-07-21 17:52:51 -0300

    agregando formularios

--- app.js
+++ app.js
@@ -5,4 +5,6 @@ const path = require("path");
 
 app.use(express.static("public"));
 app.get("/", (req,res)=> res.sendFile(path.join(__dirname, "views", "home.html")));
+app.get("/register", (req,res)=> res.sendFile(path.join(__dirname, "views", "register.html")));
+app.get("/login", (req,res)=> res.sendFile(path.join(__dirname, "views", "login.html")));
 app.listen(port, ()=> console.log(`Server running in port ${port}`));
